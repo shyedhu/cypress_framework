979f3a8edb99efd36967b5aecfafea169c4adf06
Shyedhu <shyedhu@users.noreply.github.com>
2020-02-11 22:32:20 -0800

    updated

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,5 +1,14 @@
 import './commands'
+const addContext = require('mochawesome/addContext')
+
 //ignore uncaught exceptions
 Cypress.on('uncaught:exception', (err) =>{
     return false
+})
+
+Cypress.on('test:after:run', (test, runnable) => {
+    if (test.state === 'failed') {
+        const screenshotFileName = `${runnable.parent.title} -- ${test.title} (failed).png`
+        addContext({ test }, `assets/${Cypress.spec.name}/${screenshotFileName}`)
+    }
 })
